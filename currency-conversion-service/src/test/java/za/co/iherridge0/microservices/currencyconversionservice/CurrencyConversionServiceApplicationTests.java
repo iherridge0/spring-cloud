0e467fee8b1679fa553bbd05f21be24b64569f09
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-14 13:03:34 +0200

    Setting up Currency Conversion Microservice

---
+++ currency-conversion-service/src/test/java/za/co/iherridge0/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.iherridge0.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
