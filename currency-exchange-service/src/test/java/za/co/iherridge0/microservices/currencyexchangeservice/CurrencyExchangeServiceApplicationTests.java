a42d0b1f519ddd62c147c8d833497f7b5a119219
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-14 11:42:48 +0200

    Setting up Currency Exchange Microservice

---
+++ currency-exchange-service/src/test/java/za/co/iherridge0/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.iherridge0.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
