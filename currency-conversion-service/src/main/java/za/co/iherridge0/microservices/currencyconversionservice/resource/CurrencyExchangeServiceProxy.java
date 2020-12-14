18b94eef39ceb80cc486cbbd21afdc021d577c81
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-14 14:25:38 +0200

    Running client side load balancing with Ribbon}
    Now able to distribute the load from currency-exchange service from currency-conversion-service

--- currency-conversion-service/src/main/java/za/co/iherridge0/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/za/co/iherridge0/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import za.co.iherridge0.microservices.currencyconversionservice.entity.CurrencyConversion;
 
-//@FeignClient(name = "currency-conversion-service", url = "localhost:8000")
-@FeignClient(name = "currency-conversion-service")
-@RibbonClient(name = "currency-conversion-service")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
