8f5436933872ade86d96600cdb9cc118ad37b6c5
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-15 13:13:45 +0200

    Executing a request through Zuul API Gateway Setting up Zuul API Gateway between microservice invocations

--- currency-conversion-service/src/main/java/za/co/iherridge0/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/za/co/iherridge0/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import za.co.iherridge0.microservices.currencyconversionservice.entity.CurrencyConversion;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
