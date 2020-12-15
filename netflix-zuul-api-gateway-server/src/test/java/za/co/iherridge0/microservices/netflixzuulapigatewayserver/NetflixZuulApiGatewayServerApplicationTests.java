8b53533e29b3d1c07d3c1ca07a4fc50b29321038
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-15 12:49:55 +0200

    Setting up Zuul API Gateway

---
+++ netflix-zuul-api-gateway-server/src/test/java/za/co/iherridge0/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.iherridge0.microservices.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
