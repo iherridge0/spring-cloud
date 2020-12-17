51bcdce4a61231329100122b5f411ae664113333
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-17 08:53:23 +0200

    Fault Tolerance with Hystrix

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsCofigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsCofigurationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.Configuration;
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsCofigurationController {
@@ -17,6 +18,22 @@ public class LimitsCofigurationController {
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveLimitsConfigurations() {
+		
+		throw new RuntimeException("Not Available");
+	}
+	
+	/**
+	 * On Error
+	 * @return some default configuration back
+	 */
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		
+		return new LimitConfiguration(999,9);
 	}
 	
 }
