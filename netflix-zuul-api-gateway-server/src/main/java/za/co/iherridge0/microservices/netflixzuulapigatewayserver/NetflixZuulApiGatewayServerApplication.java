63c988b2dba7b47194b71078a5a3857a0451e633
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-26 20:16:00 +0200

    Learned how to allow corsorigins

--- netflix-zuul-api-gateway-server/src/main/java/za/co/iherridge0/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/za/co/iherridge0/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import brave.sampler.Sampler;
 
@@ -16,10 +18,20 @@ public class NetflixZuulApiGatewayServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
-	
+
 	@Bean
 	public Sampler defaultSampler() {
 		return Sampler.ALWAYS_SAMPLE;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
